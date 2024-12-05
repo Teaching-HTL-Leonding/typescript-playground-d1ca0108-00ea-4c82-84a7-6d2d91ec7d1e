97f66de5f3fc57e83fcc4a4d55850303c0bdfc2b
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 09:30:44 +0100

    Saved in TypeScript Playground at 2024-12-05T08:30:43.911Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -1,8 +1,10 @@
 let grid = 10;
+let raycolor = 0;
 
 function setup() {
     createCanvas(601, 301);
     background("black");
+    colorMode(HSB)
 
     fill("aqua");
     noStroke();
@@ -11,6 +13,7 @@ function setup() {
     textAlign(CENTER, CENTER);
     text("Move the mouse to start", 0, 0, width, height);
 }
+
 
 function mouseMoved() {
     resetMatrix();
@@ -25,17 +28,18 @@ function mouseMoved() {
         for (let x = 0; x < width; x += patternSize) {
             push();
             translate(x, y);
-            line(0, 0, patternSize, patternSize / 2);
-            line(patternSize, patternSize / 2, 0, patternSize);
+            line(0, 0, patternSize , patternSize / 2 );
+            line(patternSize , patternSize / 2, 0, patternSize);
             pop()
         }
     }
+
 
     fill("black")
     noStroke();
     rect(0, height - 30, width, 30)
     textSize(15)
     fill("white")
-    text(`${mouseX} of 601`, 50, height - 15);
+    text(`${mouseX} of 601 = ${Math.floor(mouseX / 3)}%`, 70, height - 15);
 }
 
