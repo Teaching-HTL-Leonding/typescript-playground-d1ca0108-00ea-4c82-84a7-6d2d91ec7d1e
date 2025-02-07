85a6bb57e28047b7480b3b424a490b442b3d42b7
Rainer Stropek <rainer@timecockpit.com>
2025-02-07 12:32:25 +0100

    Saved in TypeScript Playground at 2025-02-07T11:32:24.314Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,5 +1,34 @@
+let i = 0;
+let playersCount = 0;
+let all_players = []
+let randomNames = [
+    "Lucas", 
+    "Thomas", 
+    "David", 
+    "Felix", 
+    "Bob", 
+    "Timo",
+    "Bobby"
+]
+
 function setup() {
+    createCanvas(500, 400)
+    background("grey")
+    rect(0, 0, width, height / 2)
 }
 
-function draw() {
+function mouseClicked() {
+    if (mouseY < height / 2) {
+        playersCount++;
+        let randomNameSelector = randomNames[Math.floor(random(0, randomNames.length))]
+        all_players.push(randomNameSelector)
+        print(`${randomNameSelector} got added!`)
+    }
+    else if (mouseY > height / 2 && playersCount > i) {
+        print(all_players[i])
+        i++;
+    }
+    else {
+        print("No other player found")
+    }
 }
