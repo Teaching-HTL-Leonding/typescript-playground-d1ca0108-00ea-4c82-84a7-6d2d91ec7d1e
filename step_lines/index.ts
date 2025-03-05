21c95ec81cd08bca0cc0636098d0a2688e21efb0
Rainer Stropek <rainer@timecockpit.com>
2025-03-05 09:50:41 +0100

    Saved in TypeScript Playground at 2025-03-05T08:50:41.312Z

--- step_lines/index.ts
+++ step_lines/index.ts
@@ -5,38 +5,12 @@ function setup() {
     createCanvas(SIZE, SIZE);
     background("black");
 
-    strokeWeight(0.25);
-    stroke("lightgray");
-
-    // Draw vertical lines
-    let i = GRID;
-    while (i < SIZE) {
-        line(i, 0, i, SIZE);
-        i += GRID;
-    }
-
-    // Draw horizontal lines
-    i = GRID;
-    while (i < SIZE) {
-        line(0, i, SIZE, i);
-        i += GRID;
-    }
-
     strokeWeight(2);
-    
-    // Draw left part of the lines
-    stroke("yellow");
-    i = GRID;
-    while (i < SIZE) {
-        line(GRID, i, GRID * (i / GRID), i);
-        i += GRID;
-    }
 
-    // Draw right part of the lines
-    stroke("red");
-    i = GRID;
-    while (i < SIZE) {
-        line(GRID * (i / GRID), i, SIZE - GRID, i);
-        i += GRID;
+    for (let i = GRID; i < width; i += GRID) {
+        stroke("yellow")
+        line(GRID, i, i, i)
+        stroke("red")
+        line(width - GRID, i, i, i)
     }
 }
